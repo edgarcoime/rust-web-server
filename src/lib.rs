593179273b56ad5c9cf07329e7a9f121004a4a13
Edgar Coime <e.coimeii@gmail.com>
2022-03-14 18:31:05 -0700

    feat: create `workers` to define unit of work that threadpool creates

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
-  /// Create a new ThreadPool.
-  /// 
-  /// The size is the number of threads in the pool.
-  /// 
-  /// # Panics
-  /// The `new` function will panic if the size is zero.
-  pub fn new(size: usize) -> ThreadPool {
-    assert!(size > 0);
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
 
-    ThreadPool
-  }
+        let mut workers = Vec::with_capacity(size);
 
-  pub fn execute<F>(&self, f: F) 
-  where F: FnOnce() + Send + 'static {
+        for id in 0..size {
+            workers.push(Worker::new(id))
+        }
 
-  }
+        ThreadPool { workers }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
+    }
 }
