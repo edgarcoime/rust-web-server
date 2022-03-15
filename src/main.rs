cd5074795688c4b7093049f0d83046b9fef6ad07
Edgar Coime <e.coimeii@gmail.com>
2022-03-14 17:43:08 -0700

    feat: hanlde tcp connection and reading out request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::prelude;
+use std::io::prelude::*;
 
 fn main() {
     // TODO: Handle error case gracefully
@@ -8,6 +8,14 @@ fn main() {
     for stream in listener.incoming() {
         // TODO: Handle error
         let stream = stream.unwrap();
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    // TODO: handle reading error
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
 }
