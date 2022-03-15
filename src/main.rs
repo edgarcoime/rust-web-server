b4659f0c5a928a6c47362546b93e7e397b943077
Edgar Coime <e.coimeii@gmail.com>
2022-03-14 17:39:15 -0700

    feat: setup basic http via tcp stream conn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Handle error case gracefully
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        // TODO: Handle error
+        let stream = stream.unwrap();
+        println!("Connection established");
+    }
 }
